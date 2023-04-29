879fb8795b9ccbe720db05db071884bc02c45aa6
yumi0507 <michellechang958@gmail.com>
2023-04-29 15:53:44 +0800

    新增遊戲需要變數

---
+++ WindowsFormsApp1/Manager/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.Manager;
+using Tile;
+
+namespace Game.Manager
+{
+    internal class GameManager
+    {
+        int boom;
+        List<ClientState> players;
+
+        public int setBoom()
+        {
+            if(players.Count == 4)
+            {
+                boom = 3;
+            }
+            else if(players.Count == 3) 
+            {
+                boom = 4;
+            }
+            else if(players.Count == 2)
+            {
+                boom = 5;
+            }
+
+            return boom;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Manager/Tile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tile
+{
+    internal class Map
+    {
+        bool[,] map;
+        int ROW;
+        int COL;
+
+        public Map() { 
+            map = new bool[6, 6]{
+                { false, false, false, false, false, false },
+                { false, false, false, false, false, false },
+                { false, false, false, false, false, false },
+                { false, false, false, false, false, false },
+                { false, false, false, false, false, false },
+                { false, false, false, false, false, false }
+            };
+            ROW = 6; COL = 6;
+        }
+
+        public int getROW()
+        {
+            return ROW;
+        }
+
+        public int getCOL() 
+        { 
+            return COL;
+        }
+
+    }
+}
